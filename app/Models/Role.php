f48b0954034ea0538d86b175d718485c143145b3
jcontrerasa9 <jacontrerasa@unal.edu.co>
2026-04-27 09:41:24 -0500

    feat: relacion entre users y roles

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,7 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\User;
+
 class Role extends Model
 {
-    //
+    public function users(){
+        $this->hasMany(User::class);
+    }
 }
