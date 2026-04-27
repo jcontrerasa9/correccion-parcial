0fbbad9385a56a465d56616d7a42fc05619a80f4
jcontrerasa9 <jacontrerasa@unal.edu.co>
2026-04-27 09:53:39 -0500

    fix: id de role por default en 0

--- database/migrations/2026_04_27_143604_add_role_to_users.php
+++ database/migrations/2026_04_27_143604_add_role_to_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('role_id')->after('id');
+            $table->unsignedBigInteger('role_id')->after('id')->default(0);
             $table->foreign('role_id')->references('id')->on('roles');
         });
     }
